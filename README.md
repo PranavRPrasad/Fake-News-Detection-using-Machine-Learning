3c92bb31f2ec768f773b170dbc9643a339cd0693
Pranav R Prasad <pranavs@Pranavs-MacBook-Pro.local>
2024-05-18 18:33:35 -0700

    Added the code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Fake-News-Detection-using-Machine-Learning
+# Gist
++ Analyzed an extensive dataset, nearly 1gB in size, encompassing labeled data (fake or real) about US elections and designed a basic website that classified the text given by the user (input) as fake news or real news (output).
+  + Tech Stack: HTML, CSS, Flask, Python.
++ Split the dataset into 70% training data and 30% testing data. Implemented various models, namely Naive Bayes, Decision Trees, Random Forest, SVM, Logistic Regression, and Passive-Aggressive Classifier on the training data by applying count-vectorization and tf-idf techniques and calculated their accuracy using confusion matrices. SVM and Passive-Aggressive Classifier provided the highest accuracy (>90%) among them.
+
+# Prerequisites
++ You can run the Notebook directly on Google Colab.
++ The dataset is already provided in the code (check for the file 'Fake_news.csv').
---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+app = Flask(__name__)
+tfvect = TfidfVectorizer(stop_words='english', max_df=0.7)
+loaded_model = pickle.load(open('pac.pkl', 'rb'))
+dataframe = pd.read_csv('Fake_News.csv')
+x = dataframe['text']
+y = dataframe['label']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+def news_detection(news):
+    tfid_x_train = tfvect.fit_transform(x_train)
+    tfid_x_test = tfvect.transform(x_test)
+    input_data = [news]
+    vectorized_input_data = tfvect.transform(input_data)
+    prediction = loaded_model.predict(vectorized_input_data)
+    return prediction
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/model')
+def model():
+    return render_template('model.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        pred = news_detection(message)
+        print(pred)
+        return render_template('test.html', prediction=pred)
+    else:
+        return render_template('test.html', prediction="Something went wrong")
+
+if __name__ == '__main__':
+    app.run(debug=True)
